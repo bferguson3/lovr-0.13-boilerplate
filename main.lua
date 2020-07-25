bc2ed233815dd773db0e00393112d1e037a0f923
Ben Ferguson <shram@Bens-MacBook-Air.local>
2020-07-25 16:31:31 -0400

    update

--- main.lua
+++ main.lua
@@ -17,51 +17,10 @@ function lovr.load()
     sOperatingSystem = lovr.getOS()
     debug.print('OS detected: ' .. sOperatingSystem)
     
-    -- set up shader
-    defaultVertex = [[
-        out vec3 FragmentPos;
-        out vec3 Normal;
-
-        vec4 position(mat4 projection, mat4 transform, vec4 vertex) { 
-            Normal = lovrNormal;
-            FragmentPos = (lovrModel * vertex).xyz;
-        
-            return projection * transform * vertex;
-        }
-    ]]
-    defaultFragment = [[
-        uniform vec4 liteColor;
-
-        uniform vec4 ambience;
+    -- set up shaders
+    defaultVertex = lovr.filesystem.read('src/default.vs')
+    defaultFragment = lovr.filesystem.read('src/default.fs')
     
-        in vec3 Normal;
-        in vec3 FragmentPos;
-        uniform vec3 lightPos;
-
-        uniform vec3 viewPos;
-        uniform float specularStrength;
-        uniform float metallic;
-        
-        vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) 
-        {    
-            //diffuse
-            vec3 norm = normalize(Normal);
-            vec3 lightDir = normalize(lightPos - FragmentPos);
-            float diff = max(dot(norm, lightDir), 0.0);
-            vec4 diffuse = diff * liteColor;
-            
-            //specular
-            vec3 viewDir = normalize(viewPos - FragmentPos);
-            vec3 reflectDir = reflect(-lightDir, norm);
-            float spec = pow(max(dot(viewDir, reflectDir), 0.0), metallic);
-            vec4 specular = specularStrength * spec * liteColor;
-            
-            vec4 baseColor = graphicsColor * texture(image, uv);            
-            //vec4 objectColor = baseColor * vertexColor;
-
-            return baseColor * (ambience + diffuse + specular);
-        }
-    ]]
     shader = lovr.graphics.newShader(defaultVertex, defaultFragment, {})
     
     -- Set default shader values
